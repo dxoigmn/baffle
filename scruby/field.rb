9268566e7ecc272cb201fa6a33d17090b07d204f
Daniel G. Peebles <Daniel.G.Peebles@Dartmouth.edu>
2007-05-03 19:15:47 +0000

    this is getting fugly

--- scruby/field.rb
+++ scruby/field.rb
@@ -11,192 +11,327 @@
 # General Public License for more details. 
 
 module Scruby
-class Field
+  class Field
 
-  attr_accessor :name
-  attr_accessor :default_value
-  attr_accessor :format
+    attr_accessor :name
+    attr_accessor :default_value
+    attr_accessor :format
 
-# Constructor
-def initialize(name, default_value)
-  @name = name
-  @default_value = default_value
-  @format = ''
-  
-  self.init()
-end
+    # Constructor
+    def initialize(name, default_value)
+      @name = name
+      @default_value = default_value
+      @format = ''
 
-# Field initialization. This function have to be redefined by subclasses.
-def init
-end
+      self.init()
+    end
 
-# Converts from network to internal encoding
-# e.g for IP->{dst}: number 2130706433 -> string "127.0.0.1" (2130706433 = 127*2^24 + 1*2^0)
-def fromnet(value)
-  return value[0]
-end
+    # Field initialization. This function have to be redefined by subclasses.
+    def init
+    end
 
-# Converts from internal encoding to network
-# e.g. for IP->{dst}: string "127.0.0.1"-> number 2130706433
-def tonet(value)
-  return [value].pack(@format)
-end
+    # Converts from network to internal encoding
+    # e.g for IP->{dst}: number 2130706433 -> string "127.0.0.1" (2130706433 = 127*2^24 + 1*2^0)
+    def fromnet(value)
+      return value[0]
+    end
 
-# Converts from internal encoding to human display
-# e.g. displays "0xDEADBEEF" for checksums
-def tohuman(value)
-  return value.to_s
-end
+    # Converts from internal encoding to network
+    # e.g. for IP->{dst}: string "127.0.0.1"-> number 2130706433
+    def tonet(value)
+      return [value].pack(@format)
+    end
 
-end
+    # Converts from internal encoding to human display
+    # e.g. displays "0xDEADBEEF" for checksums
+    def tohuman(value)
+      return value.to_s
+    end
 
-# Field for a string
-class StrField<Field
-
-def init
-  @format = 'A*'
-end
-
-def tonet(value)
-  return value.to_s
-end
-
-def tohuman(value)
-  return value.to_s.inspect
-end
-
-end
-
-# Field for one byte
-class ByteField<Field
-
-def init
-  @format = 'C'
-end
-
-end
-
-# Same as ByteField, displayed in hexadecimal form
-class XByteField<ByteField
-
-def tohuman(value)
-  return sprintf('0x%x', value)
-end
-
-end
-
-# Field for one short (big endian/network order)
-class ShortField<Field
-
-def init
-  @format = 'n'
-end
-
-end
-
-# Same as ShortField, displayed in hexadecimal form
-class XShortField<ShortField
-
-def tohuman(value)
-  return sprintf('0x%x', value)
-end
-
-end
-
-# Field for one long (big endian/network order)
-class LongField<Field
-
-def init
-  @format = 'N'
-end
-
-end
-
-# Field for one long (little endian order)
-class LELongField<LongField
-
-def init
-  @format = 'V'
-end
-
-end
-
-# Field for one long (host order)
-class HostOrderLongField<LongField
-
-def init
-  @format = 'L_'
-end
-
-end
-
-# Field for one integer
-class IntField<Field
-
-def init
-  @format = 'I'
-end
-
-end
-
-# Field for an IP address
-class IPField<Field
-
-def init
-  @format = 'N'
-  @ip_addr = nil
-end
-
-# Ruby equivalent to inet_aton. It takes a hostname or an IP as an argument.
-def inet_aton(name)
-  ip = Socket.getaddrinfo(name, nil)[0][3]
-  return [IPAddr.new(ip).to_i].pack(@format)
-end
-
-def tonet(value)
-  
-  # Getting the IP address from the server name if needed
-  if @ip_addr.nil?
-    @ip_addr = inet_aton(value)
-  end
-  
-  return @ip_addr
-end
-
-def fromnet(value_array)
-  return IPAddr.new(value_array[0], Socket::AF_INET).to_s
-end
-
-end
-
-# Field for an MAC address
-class MACField<Field
-
-def init
-  @format = 'H2H2H2H2H2H2'
-end
-
-def tonet(value)
-  # value can be empty (e.g. loopback device)
-  if value.nil?
-    value = '00:00:00:00:00:00'
   end
 
-  # Get the bytes in an string array
-  bytes = value.split(':')
+  # Field for a string
+  class StrField < Field
 
-  return bytes.pack(@format)
-end
+    def init
+      @format = 'A*'
+    end
 
-def fromnet(value)
-  # value is an array containing 6 bytes
-  return value.join(':')
-end
+    def tonet(value)
+      return value.to_s
+    end
 
-end
+    def tohuman(value)
+      return value.to_s.inspect
+    end
 
-# One day, this will be computed automatically :)
-FIELD_LIST = [StrField, ByteField, XByteField, ShortField,
-              XShortField, LongField, LELongField, HostOrderLongField,
-              IntField, IPField, MACField]
+  end
+
+  # Field for one byte
+  class ByteField<Field
+
+    def init
+      @format = 'C'
+    end
+
+  end
+
+  # Same as ByteField, displayed in hexadecimal form
+  class XByteField<ByteField
+
+    def tohuman(value)
+      return sprintf('0x%x', value)
+    end
+
+  end
+
+  # Field for one short (big endian/network order)
+  class ShortField<Field
+
+    def init
+      @format = 'n'
+    end
+
+  end
+
+  class LEShortField < Field
+    def init
+      @format = 'v'
+    end
+  end
+
+  # Same as ShortField, displayed in hexadecimal form
+  class XShortField<ShortField
+
+    def tohuman(value)
+      return sprintf('0x%x', value)
+    end
+
+  end
+
+  # Field for one long (big endian/network order)
+  class LongField<Field
+
+    def init
+      @format = 'N'
+    end
+
+  end
+
+  # Field for one long (little endian order)
+  class LELongField<LongField
+
+    def init
+      @format = 'V'
+    end
+
+  end
+
+  # Field for one long (host order)
+  class HostOrderLongField<LongField
+
+    def init
+      @format = 'L_'
+    end
+
+  end
+
+  # Field for one integer
+  class IntField<Field
+
+    def init
+      @format = 'I'
+    end
+
+  end
+
+  # Field for an IP address
+  class IPField<Field
+
+    def init
+      @format = 'N'
+      @ip_addr = nil
+    end
+
+    # Ruby equivalent to inet_aton. It takes a hostname or an IP as an argument.
+    def inet_aton(name)
+      ip = Socket.getaddrinfo(name, nil)[0][3]
+      return [IPAddr.new(ip).to_i].pack(@format)
+    end
+
+    def tonet(value)
+
+      # Getting the IP address from the server name if needed
+      if @ip_addr.nil?
+        @ip_addr = inet_aton(value)
+      end
+
+      return @ip_addr
+    end
+
+    def fromnet(value_array)
+      return IPAddr.new(value_array[0], Socket::AF_INET).to_s
+    end
+
+  end
+
+  # Field for an MAC address
+  class MACField < Field
+
+    def init
+      @format = 'H2H2H2H2H2H2'
+    end
+
+    def tonet(value)
+      # value can be empty (e.g. loopback device)
+      if value.nil?
+        value = '00:00:00:00:00:00'
+      end
+
+      # Get the bytes in an string array
+      bytes = value.split(':')
+
+      return bytes.pack(@format)
+    end
+
+    def fromnet(value)
+      # value is an array containing 6 bytes
+      return value.join(':')
+    end
+
+  end
+  
+  class BitField < Field
+    attr_accessor :size
+    
+    def initialize(name, default, size)
+      super(name, default)
+      self.size = size
+    end
+
+    def addfield(pkt, s, val)
+      if val.nil?
+        val = 0
+      end
+
+      if s.kind_of?(Array)
+        s, bitsdone, v = s
+      else
+        bitsdone = 0
+        v = 0
+      end
+
+      v <<= self.size
+      v |= val & ((1 << self.size) - 1)
+      bitsdone += self.size
+      while bitsdone >= 8
+        bitsdone -= 8
+        s = s + [v >> bitsdone].pack("C")
+        v &= (1<<bitsdone) - 1
+      end
+
+      if bitsdone
+        return s,bitsdone,v
+      else
+        return s
+      end
+    end
+
+    def getfield(pkt, s)
+      if s.kind_of?(Array)
+        s,bn = s
+      else
+        bn = 0
+      end
+
+      # we don't want to process all the string
+      nb_bytes = (self.size+bn-1)/8 + 1
+      w = s[0, nb_bytes]
+
+      # split the substring byte by byte
+      bytes = w.unpack('%dC' % nb_bytes)
+
+      b = 0
+      nb_bytes.times do |c|
+        b |= long(bytes[c]) << (nb_bytes-c-1)*8
+      end
+
+      # get rid of high order bits
+      b &= (1 << (nb_bytes*8-bn)) - 1
+
+      # remove low order bits
+      b = b >> (nb_bytes*8 - self.size - bn)
+
+      bn += self.size
+      s = s[bn / 8..-1]
+      bn = bn % 8
+      if bn
+        return [s,bn],b
+      else
+        return s,b
+      end
+    end
+
+    def randval
+      return RandNum(0,2**self.size-1)
+    end
+  end
+
+  class FlagsField < BitField
+    attr_accessor :multi, :names
+    
+    def initialize(name, default, size, names)
+      super(name, default, size)
+      self.multi = names.kind_of?(Array)
+      if self.multi
+        self.names = names.map {|x| [x]}
+      else
+        self.names = names
+      end
+    end
+
+    def any2i(pkt, x)
+      if x.kind_of?(String)
+        if self.multi
+          x = x.split("+").map {|y| [y]}
+        end
+
+        y = 0
+        x.each do |i| #for i in x
+          y |= 1 << self.names.index(i)
+        end
+
+        x = y
+      end
+      return x
+    end
+
+    def i2repr(pkt, x)
+      if self.multi
+        r = []
+      else
+        r = ""
+      end
+
+      i=0
+      while x != 0
+        if x & 1
+          r += self.names[i]
+        end
+        i += 1
+        x >>= 1
+      end
+
+      if self.multi
+        r = r.join('+')
+      end
+
+      return r
+    end
+  end
+
+  # One day, this will be computed automatically :)
+  FIELD_LIST = [StrField, ByteField, XByteField, ShortField, LEShortField,
+    XShortField, LongField, LELongField, HostOrderLongField,
+    IntField, IPField, MACField, BitField, FlagsField]
 end
--- scruby_ng/scruby_ng.rb
+++ scruby_ng/scruby_ng.rb
@@ -1,6 +1,91 @@
 require 'bit-struct'
 
 class BitStruct
+  class Field
+    attr_reader :applicable
+    
+    def offset
+      if @offset.kind_of?(Proc)
+        @offset.call(@prev)
+      else
+        @offset
+      end
+    end
+    
+    def length
+      if @applicable and !@applicable.call
+        return 0
+      end
+      
+      return @length
+    end
+    
+    def inspectable?
+      if @applicable
+        @applicable.call
+      else
+        true
+      end
+    end
+    
+    # Options are _display_name_, _default_, and _format_ (subclasses of Field
+    # may add other options).
+    def initialize(prev, offset, length, name, opts = {})
+      @prev, @offset, @length, @name, @options =
+        prev, offset, length, name, opts
+      
+      @display_name = opts[:display_name] || opts["display_name"]
+      @default      = opts[:default] || opts["default"] || self.class.default
+      @format       = opts[:format] || opts["format"]
+      @applicable   = opts[:applicable] || opts["applicable"]
+    end
+  end
+  
+  class CharField < Field
+    def add_accessors_to(cl, attr = name) # :nodoc:
+      unless offset % 8 == 0
+        raise ArgumentError,
+          "Bad offset, #{offset}, for #{self.class} #{name}." +
+          " Must be multiple of 8."
+      end
+      
+      unless length % 8 == 0
+        raise ArgumentError,
+          "Bad length, #{length}, for #{self.class} #{name}." +
+          " Must be multiple of 8."
+      end
+      
+      field = self
+      
+      cl.class_eval do
+        define_method attr do ||
+          offset_byte = field.offset(self) / 8
+          length_byte = field.length(self) / 8
+          last_byte = offset_byte + length_byte - 1
+          byte_range = offset_byte..last_byte
+          val_byte_range = 0..length_byte-1
+
+          self[byte_range].to_s
+        end
+
+        define_method "#{attr}=" do |val|
+          offset_byte = field.offset(self) / 8
+          length_byte = field.length(self) / 8
+          last_byte = offset_byte + length_byte - 1
+          byte_range = offset_byte..last_byte
+          val_byte_range = 0..length_byte-1
+
+
+          val = val.to_s
+          if val.length < length_byte
+            val += "\0" * (length_byte - val.length)
+          end
+          self[byte_range] = val[val_byte_range]
+        end
+      end
+    end
+  end
+  
   class BitEnumField < UnsignedField
     def BitEnumField.class_name
       "BitEnumField"
@@ -8,8 +93,7 @@ class BitStruct
     
     def initialize(*args)
       super(*args)
-      
-      @spec = args[3][:spec]
+      @spec = args[4][:spec]
     end
     
     def inspect_in_object(obj, opts)
@@ -27,7 +111,7 @@ class BitStruct
     def initialize(*args)
       super(*args)
       
-      @spec = args[3][:spec]
+      @spec = args[4][:spec]
     end
     
     def inspect_in_object(obj, opts)
@@ -53,6 +137,57 @@ class BitStruct
       opts = parse_options(rest, name, FlagField)
       add_field(name, length, opts)      
     end
+    
+    # Add a field to the BitStruct (usually, this is only used internally).
+    def add_field(name, length, opts = {})
+      round_byte_length ## just to make sure this has been calculated
+      ## before adding anything
+      p opts
+      name = name.to_sym
+      
+      if @closed
+        raise ClosedClassError, "Cannot add field #{name}: " +
+          "The definition of the #{self.inspect} BitStruct class is closed."
+      end
+
+      if fields.find {|f|f.name == name}
+        raise FieldNameError, "Field #{name} is already defined as a field."
+      end
+
+      if instance_methods(true).find {|m| m == name}
+        if opts[:allow_method_conflict] || opts["allow_method_conflict"]
+          warn "Field #{name} is already defined as a method."
+        else
+          raise FieldNameError,"Field #{name} is already defined as a method."
+        end
+      end
+      
+      field_class = opts[:field_class]
+      
+      prev = fields[-1] || NULL_FIELD
+      
+      if prev.applicable
+        offset = proc { |prev| prev.offset + prev.length }
+      else
+        offset = prev.offset + prev.length
+      end
+      
+      field = field_class.new(prev, offset, length, name, opts)
+      field.add_accessors_to(self)
+      fields << field
+      own_fields << field
+      @bit_length += field.length
+      @round_byte_length = (bit_length/8.0).ceil
+
+      if @initial_value
+        diff = @round_byte_length - @initial_value.length
+        if diff > 0
+          @initial_value << "\0" * diff
+        end
+      end
+
+      field
+    end
   end
 end
 
@@ -63,29 +198,10 @@ class Dot11 < BitStruct
   flag_field :fc_field, 8, 'FCField', :spec => ["to-DS", "from-DS", "MF", "retry", "pw-mgt", "MD", "wep", "order"]
   unsigned :id, 16, 'ID'
   hex_octets :addr1, 48, 'Address 1'
-  hex_octets :addr2, 48, 'Address 2'
+  hex_octets :addr2, 48, 'Address 2', :applicable => proc { |parent| if parent.type == 1 then [0xb, 0xa, 0xe, 0xf].include?(parent.subtype) else true end }
   hex_octets :addr3, 48, 'Address 3'
   unsigned :sc, 16, 'SC', :endian => :little
   hex_octets :addr4, 48, 'Address 4'
- 
-=begin 
-  def init
-    @protocol = 'TCP'
-    @fields_desc = [ 
-      BitField('subtype', 0, 4),
-      BitEnumField("type", 0, 2, ["Management", "Control", "Data", "Reserved"]),
-      BitField("proto", 0, 2),
-      FlagsField("FCfield", 0, 8, ["to-DS", "from-DS", "MF", "retry", "pw-mgt", "MD", "wep", "order"]),
-      ShortField("ID",0),
-      MACField("addr1", ETHER_ANY),
-      Dot11Addr2MACField("addr2", ETHER_ANY),
-      Dot11Addr3MACField("addr3", ETHER_ANY),
-      Dot11SCField("SC", 0),
-      Dot11Addr4MACField("addr4", ETHER_ANY) 
-    ]      
-  end
-=end
-
 end
 
 a = Dot11.new
