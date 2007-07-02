2e52054d30e51c1192053deda1f8fb6f66bb5d62
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2007-07-02 19:47:54 +0000

    Switched to capture queue.

--- baflle.rb
+++ baflle.rb
@@ -9,75 +9,120 @@ require "timeout"
 INTERFACE = "ath0"
 DRIVER = "madwifing"
 
+#  require 'thread'
+#
+#  queue = Queue.new
+#
+#  producer = Thread.new do
+#    5.times do |i|
+#      sleep rand(i) # simulate expense
+#      queue << i
+#      puts "#{i} produced"
+#    end
+#  end
+#
+#  consumer = Thread.new do
+#    5.times do |i|
+#      value = queue.pop
+#      sleep rand(i/2) # simulate expense
+#      puts "consumed #{value}"
+#    end
+#  end
+#
+#  consumer.join
+
+class CaptureQueue < Queue
+  def initialize(device)
+    super()
+    @capture = false
+    @thread = Thread.new do
+      params = { :device => device }
+      
+      Capture.open(params) do |capture|
+        capture.each do |pkt|
+          if @capture
+            self.push pkt
+          end
+        end
+      end
+    end
+  end
+  
+  def start
+    self.clear
+    @capture = true
+  end
+  
+  def stop
+    @capture = false
+  end
+end
+
 module Baflle
   def add_rule(name, args)
     @rules ||= {}
     @rules[name] = args
   end
   
-  def eval_rule(device, rule, packet)
-    sent = false
-    captured_packet = nil
-  
-    # Setup capture thread
-    capture = Thread.new do
-      params = {:device => INTERFACE}
-      params[:timeout] = rule[:timeout] if rule[:timeout]
-      params[:filter] = rule[:filter] if rule[:filter].kind_of?(String)
-      
-      puts "Opening capture..."
-      
-      Capture.open params do |capture|
-        capture.each do |pkt|
-          #puts "Received packet!"
-          if sent
-            puts "Got response!"
-            captured_packet = pkt 
-            break
-          end
-        end
-      end
-    end
-  
-    sleep 2
-  
-    # Send packet
-    puts "Writing packet..."
-    device.write(packet.data, 1, 0)
-    sent = true
-    puts "Waiting for response..."
-    
-    # Wait for capture thread to return
-    capture.join
-  
-    # Process packet
-    next_rule = (captured_packet || 
-                (rule[:expect].kind_of?(PacketSet) && 
-                  rule[:expect].include?(captured_packet))) ?
-                rule[:pass] : rule[:fail]
-  
-    # Evaluate next rule/proc
-    case next_rule.class.name
-      when "Symbol"
-        eval_rule @rules[next_rule]  # TODO: Check for nil
-      when "Proc"
-        next_rule.call
-    end
-  end
-  
-  def eval_rules
+  def eval(name)
     device = Lorcon::Device.new(INTERFACE, DRIVER, 1)
-  
-    @rules.each do |name, rule|
-      if rule[:send].kind_of? PacketSet
+    capture = CaptureQueue.new(INTERFACE)
+    rule = @rules[name]
+
+    case rule[:send]
+      when PacketSet
+        return_values = []
+        
         rule[:send].each do |params|
           packet_class = params[:class]
           params.delete :class
-          eval_rule device, rule, packet_class.new(params)
+          return_values << eval_rule(device, capture, rule, packet_class.new(params))
         end
-      else
-        eval_rule device, rule, rule[:send]
+        
+        return return_values
+      when Packet
+        return eval_rule(device, capture, rule, rule[:send])
+    end
+  end
+  
+  private
+  
+  def eval_rule(device, capture, rule, packet)
+    # Send packet
+    device.write(packet.data, 1, 0)
+    capture.start
+    
+    # Wait for response, timing out as necessary
+    response = nil
+    
+    begin
+      Timeout::timeout(rule[:timeout] || 10) do
+        while response == nil
+          response = capture.pop
+          
+          case rule[:expect]
+            when PacketSet
+              response == nil if !rule[:expect].include?(response)
+            when Packet
+              response == nil if response != rule[:expect]
+          end
+        end
       end
+    rescue Timeout::Error
+      response = nil
+    end
+    
+    # Process packet
+    next_rule = response != nil ? rule[:pass] : rule[:fail]
+  
+    # Evaluate next rule/proc
+    case next_rule
+      when Symbol
+        eval_rule @rules[next_rule]  # TODO: Check for nil
+      when String
+        return next_rule
+      when Proc
+        return next_rule.call
     end
   end
 end
--- test.rb
+++ test.rb
@@ -3,18 +3,18 @@ require "baflle"
 add_rule :rule1,
          :send => PacketSet.new(:class => Dot11),
          :expect => PacketSet.new(:class => Dot11), # or string bpf filter!
-         :pass => Proc.new { puts "Linksys" },
+         :pass => "Linksys",
          :fail => :rule2,  # timeout, or not match packet
-         :timeout => 5000
+         :timeout => 5
 
 add_rule :rule2,
          :send => PacketSet.new(:class => Dot11),
          :expect => PacketSet.new(:class => Dot11), # of string bpf filter
-         :pass => Proc.new { puts "Aruba" },
-         :fail => Proc.new { puts "Unknown" },
-         :timeout => 5000
+         :pass => "Aruba",
+         :fail => "Unknown",
+         :timeout => 5
 
-puts eval_rules
+puts eval(:rule1)
 
 
 # expect <= if PacketSet, assume ruby filtering, if String assume tcpdump filter
