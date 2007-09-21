1007696cb9e04f9d45ae50b6d9318a4b1111716b
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2007-09-21 04:54:50 +0000

    Updated fake ap.

--- fake_ap.rb
+++ fake_ap.rb
@@ -8,17 +8,13 @@ require 'baflle-recv'
 trap "INT" do
   puts "exiting"
   puts "clients"
-  p $clients
-  puts "auth_counts"
-  p $auth_counts
-  puts "deauth_counts"
-  p $deauth_counts
+  p $aps
   exit
 end
 
-$ap = "ba:aa:ad:f0:00:0d"
+#$ap = "ba:aa:ad:f0:00:0d"
 
-$evil_probe_response = Dot11.new(
+$probe_response = Dot11.new(
                         :subtype =>   0x5,
                         :type =>      0x0,
                         :version =>   0x0,
@@ -48,10 +44,10 @@ $beacon = Dot11.new(:type     => 0x0,
                     :flags    => 0x0,
                     :duration => 0x0,
                     :addr1    => "ff:ff:ff:ff:ff:ff",
-                    :addr2    => "ba:aa:ad:f0:00:0d",
-                    :addr3    => "ba:aa:ad:f0:00:0d",
+                    :addr2    => 0,
+                    :addr3    => 0,
                     :sc       => 0x0000) /
-          Dot11ProbeResp.new(:timestamp => 0, :beacon_interval => 0x0064, :capabilities => 0x0100) /
+          Dot11Beacon.new(:timestamp => 0, :beacon_interval => 0x0064, :capabilities => 0x0100) /
           Dot11Elt.new(
             :id =>            0x00,
             :info_length =>   4,
@@ -65,79 +61,82 @@ $beacon = Dot11.new(:type     => 0x0,
             :info_length =>   0x01,
             :info =>          [0x0b].pack("c*"))
 
-$go_away = Dot11.new(
-             :subtype =>  0x1,
-             :type =>     0x0,
-             :version =>  0x0,
-             :flags =>    0x0,
-             :duration => 0x0,
-             :addr1 =>    0x0,
-             :addr2 =>    0x0,
-             :addr3 =>    0x0,
-             :sc =>       0x0) /
-             Dot11AssoResp.new(:capabilities => 0x0401, :status => 12, :aid => 0)
-
-$clients = {}
-$auth_counts = Hash.new(0)
-$deauth_counts = Hash.new(0)
-
 def generate_address(parameter)
   MACAddress.new(0xbaaaad000000 | (parameter & 0xffffff))
 end
+
+@device = Lorcon::Device.new("ath0", "madwifing", 1)
 
 def emit(packet)
   @device.write(packet, 1, 0)
 end
 
-@device = Lorcon::Device.new("ath0", "madwifing", 1)
+$current_flag       = 0x00
+$flags_responded    = Hash.new(0)
+$num_beacons_sent   = 0
+$last_current_flag  = 0
 
-  #else
-  #  if packet.addr1[0] != 0xff && $clients[packet.addr2] == packet.addr1
-  #    if packet.subtype == 11
-  #      $go_away.addr1 = packet.addr2
-  #      $go_away.addr2 = packet.addr1
-  #      $go_away.addr3 = packet.addr1
-  #      
-  #      emit $go_away.data
-  #    end
-  #  end
-  #end
-
+# Log auth requests
 Thread.new do
+  $retry_count = 0
   sniff :device => 'ath0', :filter => 'wlan[0] == 0xb0' do |packet|
-    next unless $clients[packet.addr2] == packet.addr1
+    ap_address = generate_address($current_flag)
     
-    $go_away.addr1 = packet.addr2
-    $go_away.addr2 = packet.addr1
-    $go_away.addr3 = packet.addr1
+    next unless packet.addr1 == MACAddress.new('ff:ff:ff:ff:ff:ff') ||
+                packet.addr1 == ap_address
 
-    emit $go_away.data
+    $flags_responded[packet.addr1.to_i & 0xff] += 1    
     
-    puts "telling #{packet.addr2} to go away with #{packet.addr1}!"
+    $retry_count += 1 if packet.flags & 0x08 == 0x08
+    $retry_count  = 0 if packet.flags & 0x08 == 0x00
+    
+    puts "#{packet.addr2} tried to authenticate with #{packet.addr1}!"  unless packet.flags & 0x08 == 0x08
+    puts "#{packet.addr2} tried to authenticate with #{packet.addr1} (retry #{$retry_count})!" if packet.flags & 0x08 == 0x08
   end
 end
 
+# Beacon broadcast every 0.25 seconds
 Thread.new do
   while true
-    $clients.each do |blah, ap_address|
-      $beacon.addr2 = ap_address
-      $beacon.addr3 = ap_address
-      emit $beacon.data
+    if $num_beacons_sent > 40
+      #$current_flag      += 1
     end
-    sleep 1
+
+    if ($last_current_flag != $current_flag)
+      $num_beacons_sent   = 0
+      $last_current_flag  = $current_flag
+    end
+
+    $beacon.flags = $current_flag  
+    $beacon.addr2 = generate_address($current_flag)
+    $beacon.addr3 = generate_address($current_flag)
+
+    #puts "Sending beacon with #{$beacon.addr2.to_s}"
+
+    emit $beacon.data
+    $num_beacons_sent += 1
+    
+    sleep 0.25
   end
 end
 
+# Look for probe requests
 sniff :device => 'ath0', :filter => 'wlan[0] == 0x40' do |packet|
-    ap_address = $clients[packet.addr2] || generate_address($clients.size)
+    ap_address = generate_address($current_flag)
+    
+    next unless packet.addr1 == MACAddress.new('ff:ff:ff:ff:ff:ff') ||
+                packet.addr1 == ap_address
+    #next if $seen
 
-    $evil_probe_response.addr1 = packet.addr2
-    $evil_probe_response.addr2 = ap_address
-    $evil_probe_response.addr3 = ap_address
-    $evil_probe_response.payload.timestamp = (Time.now.to_f * 100).to_i
+    $probe_response.flags = $current_flag
+    $probe_response.addr1 = packet.addr2
+    $probe_response.addr2 = ap_address
+    $probe_response.addr3 = ap_address
+    $probe_response.payload.timestamp = (Time.now.to_f * 100).to_i
     
-    emit $evil_probe_response.data
+    emit $probe_response.data
     
-    puts "responding to probe request from #{packet.addr2} with #{ap_address}!"
-    $clients[packet.addr2] = ap_address
+    puts "responding to probe request from #{packet.addr2} with #{ap_address}!" unless packet.flags & 0x08 == 0x08
+    puts "responding to probe request from #{packet.addr2} with #{ap_address} (retry)!" if packet.flags & 0x08 == 0x08
 end
+
