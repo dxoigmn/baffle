4338bb8548d8c4b0e03b2ef1b4dfbbe59230c3ac
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-02-07 14:15:33 +0000

    Refactor BaffleOptions into Baffle::Options in options.rb

--- tool/tool.rb
+++ tool/tool.rb
@@ -1,53 +1,8 @@
 #!/usr/bin/env ruby
 
+require File.join(File.dirname(__FILE__), "options")
 require 'rubygems'
-require 'optparse'
 require 'SVM'
 
-class BaffleOptions
-  def self.parse(args)
-    options = {}
-
-    opts = OptionParser.new do |opts|
-      opts.program_name = "tool.rb"
-      opts.version      = "0.1"
-      opts.release      = "a"
-      
-      opts.banner = "Usage: #{opts.program_name} [options] ESSID|BSSID"
-
-      opts.separator("")
-      opts.separator("Fingerprinting options:")
-      opts.on("-i INTERFACE", "--interface INTERFACE", "The INTERFACE to use for both injection and capture") { |interface| options[:interface] = interface }
-      opts.on("-j INTERFACE", "--inject INTERFACE", "The INTERFACE to use for injection") { |interface| options[:inject] = interface }
-      opts.on("-c INTERFACE", "--capture INTERFACE", "The INTERFACE to use for capture") { |interface| options[:capture] = interface }
-  
-      opts.separator("")
-      opts.separator("Training options:")
-      opts.on("-t", "--train", "Train baffle with a new device fingerprint") { options[:train] = true}
-
-      opts.separator("")
-      opts.separator("Common options:")
-      opts.on("-v", "--verbose", "More detailed output") { options[:verbose] = true }
-      opts.on("-?", "--help", "Show this message") { puts opts.help; exit }
-      opts.on("--version", "Print the version") { puts opts.ver; exit }
-    end
-    
-    value = opts.parse!(args).first
-
-    if value =~ /^([0-9a-fA-F]{2}[:-]){5}[0-9a-fA-F]{2}$/
-      options[:bssid] = value
-    else
-      options[:essid] = value
-    end
-    
-    unless options[:bssid] || options[:essid]
-      puts opts.help 
-      exit
-    end
-    
-    options
-  end
-end
-
-options = BaffleOptions.parse(ARGV)
+options = Baffle::Options.parse(ARGV)
 p options
