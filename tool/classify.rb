61c2937ccdf5bef6a86f40506c7902de05375478
Daniel G. Peebles <Daniel.G.Peebles@Dartmouth.edu>
2008-02-07 19:26:14 +0000

    clarified voting mechanism

--- tool/classify.rb
+++ tool/classify.rb
@@ -7,6 +7,8 @@ module Baffle
     votes = Hash.new(0)
     total_votes = 0
     
+    # tally up the votes, giving each candidate device a vote count based on the total
+    # votes allocated to the probe and the confidence of the probe in its hypothesis
     probe_results.each do |probe_result|
       probe_result.hypotheses.each_pair do |device, confidence|
         votes[device] += probe_result.total_votes * confidence
@@ -15,9 +17,10 @@ module Baffle
       total_votes += probe_result.total_votes
     end
     
-    # return the key with the largest value in votes
+    # sort the candidates by the number of votes they received
     sorted_votes = votes.sort_by{|x| x[1]}
     
+    # only return the top candidate if it exceeds the second by the relative margin
     if sorted_votes[-1][1] > sorted_votes[-2][1] + (total_votes / VOTING_MARGIN)
       sorted_votes[-1][0]
     else
