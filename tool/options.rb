921ed7acc217fd993301318723bc93efa79530c3
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-02-11 13:45:17 +0000

    Options is now a class. Added --channel option.

--- tool/options.rb
+++ tool/options.rb
@@ -2,8 +2,28 @@ require 'optparse'
 
 module Baffle
   class Options
+    attr_accessor :inject
+    attr_accessor :capture
+    attr_accessor :driver
+    attr_accessor :channel
+    attr_accessor :train
+    attr_accessor :verbose
+
+    def initialize
+      @inject   = 'ath0'
+      @capture  = 'ath0'
+      @driver   = 'madwifing'
+      @channel  = 11
+      @train    = false
+      @verbose  = false
+    end
+    
+    def train?; self.train; end
+    def verbose?; self.verbose; end
+    def interface=(value); self.inject = value; self.capture = value; end
+    
     def self.parse(args)
-      options = {}
+      options = Options.new
 
       opts = OptionParser.new do |opts|
         opts.program_name = "tool.rb"
@@ -14,17 +34,19 @@ module Baffle
 
         opts.separator("")
         opts.separator("Fingerprinting options:")
-        opts.on("-i INTERFACE", "--interface INTERFACE", "The INTERFACE to use for both injection and capture") { |interface| options[:interface] = interface }
-        opts.on("-j INTERFACE", "--inject INTERFACE", "The INTERFACE to use for injection") { |interface| options[:inject] = interface }
-        opts.on("-c INTERFACE", "--capture INTERFACE", "The INTERFACE to use for capture") { |interface| options[:capture] = interface }
-        opts.on("-d DRIVER", "--driver DRIVER", "The driver used for injection") { |driver| options[:driver] = driver }
+        opts.on("-i INTERFACE", "--interface INTERFACE", "The INTERFACE to use for both injection and capture") { |interface| options.interface = interface }
+        opts.on("-j INTERFACE", "--inject INTERFACE", "The INTERFACE to use for injection") { |interface| options.inject = interface }
+        opts.on("-c INTERFACE", "--capture INTERFACE", "The INTERFACE to use for capture") { |interface| options.capture = interface }
+        opts.on("-d DRIVER", "--driver DRIVER", "The driver used for injection") { |driver| options.driver = driver }
+        opts.on("-c CHANNEL", "--channel CHANNEL", "The channel to listen on") { |channel| options.channel = channel }
+
         opts.separator("")
         opts.separator("Training options:")
-        opts.on("-t", "--train", "Train baffle with a new device fingerprint") { options[:train] = true }
+        opts.on("-t", "--train", "Train baffle with a new device fingerprint") { options.train = true }
 
         opts.separator("")
         opts.separator("Common options:")
-        opts.on("-v", "--verbose", "More detailed output") { options[:verbose] = true }
+        opts.on("-v", "--verbose", "More detailed output") { options.verbose = true }
         opts.on("-?", "--help", "Show this message") { puts opts.help; exit }
         opts.on("--version", "Print the version") { puts opts.ver; exit }
       end
