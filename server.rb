45331f6ebd9db95aa283ca111e2a7ea1777a6e8a
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-02-14 05:42:01 +0000

    Server now uses Ajax\!

--- server.rb
+++ server.rb
@@ -12,7 +12,7 @@ class MainController < Ramaze::Controller
       Baffle.run(["-e", essid, target, "-f", target.gsub(/:/, '_')])
     end
 
-    redirect "/image?i=#{target.gsub(/:/, '_')}_auth_flags.svg"
+    redirect "/image?i=#{target.gsub(/:/, '_')}_auth_flags.jpg"
   end
   
   def image
@@ -20,10 +20,60 @@ class MainController < Ramaze::Controller
 
     src =<<END
 <html>
+  <head>
+    <style type="text/css">
+      body {
+        background-color: #666666;
+        color: #ffffff;
+      }
+
+      .loading {
+        width: 100px;
+        height: 100px;
+        background: url(fingerprint.gif) no-repeat center center;
+      }
+    </style>
+    <script src="jquery.js"></script>
+    <script type="text/javascript">
+      function loadImage() {
+        var img = new Image();
+
+        // wrap our new image in jQuery, then:
+        $(img)
+        // once the image has loaded, execute this code
+        .load(function () {
+          // set the image hidden by default    
+          $(this).hide();
+
+          // with the holding div #loader, apply:
+          $('#loader')
+            // remove the loading class (so no background spinner), 
+            .removeClass('loading')
+            // then insert our image
+            .append(this);
+
+          // fade our image in to create a nice effect
+          $(this).fadeIn();
+          clearInterval(int);
+        })
+
+        // if there was an error loading the image, react accordingly
+        .error(function () {
+          // notify the user that the image could not be loaded
+        })
+
+        // *finally*, set the src attribute of the new image to our image
+        .attr('src', '#{image}');
+      }
+
+      var int = setInterval("loadImage()", 1000);
+    </script>
+  </head>
   <body>
-    <meta http-equiv="refresh" content="1"> 
-    moo!
-    <img src="/#{image}"/>
+    <center>
+    <h1>#{image.gsub('_', ':')}</h1>
+    <div class="loading" id="loader"></div>
+    </center>
   </body>
 </html>
 END
