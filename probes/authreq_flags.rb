88c13e215792ae97256eb948eee3492e180ad5d8
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-06-26 18:02:57 -0400

    Add auth probe.

---
+++ probes/authreq_flags.rb
@@ -0,0 +1,37 @@
+probe "authreq_flags" do
+  repeat 5
+  inject Baffle::Dot11.new(:subtype =>   0xb,
+                           :type =>      0x0,
+                           :version =>   0x0,
+                           :flags =>     0..255,
+                           :duration =>  0x0000,
+                           :addr1 =>     Baffle.options.bssid,
+                           :addr2 =>     "ba:aa:ad:f0:00:0d",
+                           :addr3 =>     Baffle.options.bssid,
+                           :sc =>        0x0000, # This is auto-filled in by the driver.
+                           :payload =>   Baffle::Dot11::Dot11Auth.new(:algo   => 0x0000,
+                                                                      :seqnum => 0x0001,
+                                                                      :status => 0x0000)) 
+  
+  capture Baffle::Dot11.new(:type => 0, :subtype => 0xb, :addr1 => "ba:aa:ad:f0:00:0d/32") do
+    1
+  end
+  
+  timeout do
+    0
+  end
+  
+  compute_vector do |samples|
+    vector = Array.new(256, 0)
+  
+    samples.each do |sample|
+      sample.each_pair do |key, value|
+        vector[key] += 1 if value != 0
+      end
+    end
+
+    vector
+  end
+
+  train "Linksys WRT56G", [5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+end
