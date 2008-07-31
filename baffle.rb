314f9e16da69fd4e803fc55aa8a93a86a7907424
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-07-31 17:46:40 -0400

    Make Baffle#run a little more friendly.

--- baffle.rb
+++ baffle.rb
@@ -6,29 +6,34 @@ require File.join(File.dirname(__FILE__), 'lib/dot11/dot11')
 
 module Baffle
   def self.run(args)
-    @options = Baffle::Options.parse(args)
-
+    options = Baffle::Options.parse(args)
+    hypotheses = {}
+    
     Baffle::Probes.each do |probe|
       puts "Running probe #{probe.name}"
-      vector = probe.run(@options)
+      vector = probe.run(options)
      
       unless vector
         warn "Probe was skipped."
         next
       end
-
-      if @options.fpdiagram
-        File.open(@options.fpdiagram + probe.name + '.svg', 'w+') do |out|
+      
+      if options.fpdiagram
+        File.open("#{options.fpdiagram}#{probe.name}.svg", 'w+') do |out|
           out.write Baffle.fingerprint_diagram(vector).to_s
         end 
       end
-
+      
       puts "Vector: #{vector.inspect}"
-
-      unless @options.train?
+      
+      hypotheses[probe.name] = probe.hypothesize(vector)
+      
+      unless options.train?
         puts "#{probe.name} hypothesizes: #{probe.hypothesize(vector)}"
       end
     end
+    
+    hypotheses
   end
 end
 
